951cbe887cfb79eef7fba92467e13ec356de105a
Arpit Raj <b123024@iiit-bh.ac.in>
2026-01-30 01:00:59 +0530

    chnaged_session_maker

--- app/databases.py
+++ app/databases.py
@@ -1,14 +1,24 @@
-import psycopg2 
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy import create_engine
+# import psycopg2 
+# from psycopg2.extras import RealDictCursor
+# import time
+from sqlalchemy import URL, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+url_object = URL.create(
+    "postgresql+psycopg2",
+    username=os.getenv("DATABASE_USERNAME"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    host=os.getenv("DATABASE_HOSTNAME"),
+    database=os.getenv("DATABASE_NAME"),
+    port=int(os.getenv("DATABASE_PORT", 5432)),
+)
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(url_object)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
